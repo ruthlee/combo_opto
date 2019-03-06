46586084e7aa7bdee4249a603226e547b21a2997
Kyelee Fitts <kyelee.fitts@gmail.com>
2019-03-06 14:59:30 +0100

    Added edge class and wweights to digraph

--- digraph_class.py
+++ digraph_class.py
@@ -2,30 +2,38 @@
 # We want methods for adding edges by inputting two integers, i.e., 1, 2 means that vertices 1 and 2 are connected.
 # since 
 
+class edge:
+    def __init__(self, v, w, weight):
+        self.v = v 
+        self.w = w
+        self.weight = weight
+
 class Digraph:
+    ''' Note that the edge will always be represented in the adjacency list as a tuple with (end_vertex, weight).
+    '''
     def __init__(self, V): # V is the number of vertices
         self.V = V
         self.adj_list = [[] for i in range(V)] # placeholder adjacency list 
     
     # add edge
-    def add_edge(self, v, w): # v, w in V
-        if v > self.V or w > self.V: 
+    def add_edge(self, e): # v, w in V, we will add a tuple to the adjacency list. e is an instance of the edge class 
+        if e.v > self.V or e.w > self.V: 
              raise ValueError('The vertex labels must be between ' + str(0) + ' and ' + str(self.V))
-        self.adj_list[v].append(w)
+        self.adj_list[e.v].append((e.w, e.weight))
     
-    def remove_edge(self, v, w):
-        if v > self.V or w > self.V: 
+    def remove_edge(self, e):
+        if e.v > self.V or e.w > self.V: 
              raise ValueError('The vertex labels must be between ' + str(0) + ' and ' + str(self.V))
-        self.adj_list[v].remove(w)
+        self.adj_list[e.v].remove((e.w, e.weight))
         
     def add_edges(self, edges):
-        # takes in a list of tuples of edges and puts them in D. 
+        # takes in a list of edge instances and puts them in D. 
         for i in range(len(edges)):
-            self.add_edge(edges[i][0], edges[i][1])
+            self.add_edge(edges[i])
     
     def remove_edges(self, edges):
         for i in range(len(edges)):
-            self.remove_edge(edges[i][0], edges[i][1])
+            self.remove_edge(edges[i])
             
     def print_digraph(self, long = False):
     # returns a summary of the digraph, with optional boolean input "long" for a more descriptive output
